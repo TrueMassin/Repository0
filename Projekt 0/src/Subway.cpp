e6eadc7a9d280e51995c3e9246a9bd8665285463
TrueMassin <44481404+TrueMassin@users.noreply.github.com>
2019-03-07 09:31:57 +0100

    Add files via upload

--- Projekt 0/src/Subway.cpp
+++ Projekt 0/src/Subway.cpp
@@ -6,7 +6,7 @@ using namespace std;
  * @author Maksymilian Malz
  */
 
- Subway::Subway()
+ Subway::Subway( unsigned int respSpeed )
  {
     for( int i = 0; i < NUMBER_OF_TRAINS; ++i ) {
         train[i] = nullptr;
@@ -16,6 +16,21 @@ using namespace std;
     }
     numberOfTrains = 0;
     numberOfPassengers = 0;
+    respawnSpeed = respSpeed;
+//DO POPRAWY@@@@@@@@@@@@@@@
+stations[0] = "Pole Mokotowskie";
+stations[1] = "Politechnika";
+stations[2] = "Plac Konstytucji";
+stations[3] = "Centrum";
+stations[4] = "Ratusz";
+stations[5] = "Muranow";
+stations[6] = "Rondo Daszynskiego";
+stations[7] = "Rondo ONZ";
+stations[8] = "Swietokrzyska";
+stations[9] = "Nowy Swiat-Uniwersytet";
+stations[10] = "Centrum Nauki Kopernik";
+stationsNumber = 11;
+//@@@@@@@@@@@@@@@@@@@@@@@@@
  }
  //******************************************
  void Subway::addTrain( string stationsList[], unsigned int tableSize )
@@ -36,9 +51,25 @@ void Subway::addPasenger( string station )
 //*******************************************
 void Subway::status()
 {
-    for( int i = 0; i < static_cast<int>(numberOfTrains); ++i) {
+    for( int i = 0; i < static_cast<int>( numberOfTrains ); ++i) {
         cout << "Pociag " << i + 1 << ":" << endl;
         train[i]->getInformation();
         cout << endl << endl;
+    }
+}
+//*****************************************
+void Subway::moveOn()
+{
+    for( unsigned int i = 0; i < numberOfTrains ; ++i ) {
+        train[i]->goToNextStation();
+    }
+}
+//******************************************
+void Subway::generatePassengers()
+{
+    unsigned int x;
+    for( unsigned int i = 0; i < respawnSpeed ; ++i ) {
+        x = rand() % stationsNumber;
+        addPasenger( stations[x] );
     }
 }
